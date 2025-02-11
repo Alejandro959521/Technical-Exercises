3c528153e03635e8751949f8fb03e526854511de
Miguel Ojeda <cabrerasm081@gmail.com>
2025-02-10 22:23:26 -0400

    .

---
+++ JavaScript/isIsomorphic.js
@@ -0,0 +1,22 @@
+
+let s = "badc"
+let t = "baba"
+
+
+var isIsomorphic = function(s, t) {
+
+dict = {}
+mapStr_t = []
+
+    for (let str_s , let str_t of s,t){
+
+
+    }
+
+
+    
+    
+};
+
+
+console.log(isIsomorphic(s, t))
--- Python/isIsomorphic.py
+++ Python/isIsomorphic.py
@@ -1,14 +1,27 @@
 
-s = "abcd"
-t = "wxyz"
+s = "badc"
+t = "baba"
 
 def isIsomorphic(s, t):
 
     dictionary = {}
+    t_map = set()
+    if len(s) != len(t):
+        return False
+    for char_s, char_t in zip(s, t):
+      
+        if char_s in dictionary:
+            if dictionary[char_s] != char_t:
+                return False
+        else:
+            if char_t in t_map:
+                return False
 
-    for x in s:
-        if x in dictionary:
-
+            dictionary[char_s] = char_t
+            t_map.add(char_t)
+  
+    return True       
+        
 
 
 
